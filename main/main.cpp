c6a395a490ac1cd27026c34960643eb104a4ed99
raging-loon <85407521+raging-loon@users.noreply.github.com>
2024-08-16 13:10:23 -0400

    added GLM

--- main/main.cpp
+++ main/main.cpp
@@ -9,17 +9,26 @@
 #include "core/rtl/hash_map.h"
 
 #include "core/rtl/iterator.h"
-
+#include <glm/vec2.hpp>
 int main(int argc, char** argv)
 {
     radium::Logger info("debug-info");
     info.setOutputSystem<radium::StdoutSystem>();
-    rtl::unordered_map<rtl::string, rtl::string> test;
+    rtl::unordered_map<int, glm::vec2> test;
+    test.reserve(10);
 
-    test.insert({ "name", "bob" });
+    printf("%0x\n", rtl::hash<int>::run(0) );
+    test.insert({ 1, {1.0f, 2.0f} });
+    
+    for (int i = 0; i < 10; i++)
+    {
+        test.insert({ i, {1.0f * i, 2.0f * i} });
+
+    }
 
     for (const auto& i : test)
-        printf("%s => %s\n",i.first.c_str(), i.second.c_str());
-    return 0;
+    {
+        printf("%d => (%f, %f)\n",i.first, i.second.x, i.second.y);
+    }
 } 
 
